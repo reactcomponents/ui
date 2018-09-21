574164083431faf57bd952aca1de1a626e188b7f
William Antony <william@willsquad.com>
2018-09-20 23:02:57 -0500

    Added Select to lib/index.js

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,5 +1,9 @@
 import Input from './components/Input/Input';
+import TextInput from './components/Input/components/TextInput/TextInput';
+import Select from './components/Input/components/Select/Select';
 
 export {
   Input,
+  TextInput,
+  Select,
 };
