785dab330719dd5c04817b7abdd7e364475b790a
William Antony <william@willsquad.com>
2018-09-21 23:24:29 -0500

    Created boilerplate for Radio

---
+++ src/lib/components/Input/components/Radio/Radio.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Radio.css';
+
+class Radio extends Component {
+
+  render() {
+    return (
+      <div className="Radio">
+
+      </div>
+    );
+  }
+
+}
+
+export default Radio;
