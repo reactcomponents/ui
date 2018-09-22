bfcaa36fed7e2b45708dbfe190038e4320e10519
William Antony <william@willsquad.com>
2018-09-21 23:24:14 -0500

    Created boilerplate for CheckBox

---
+++ src/lib/components/Input/components/CheckBox/CheckBox.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './CheckBox.css';
+
+class CheckBox extends Component {
+
+  render() {
+    return (
+      <div className="CheckBox">
+
+      </div>
+    );
+  }
+
+}
+
+export default CheckBox;
