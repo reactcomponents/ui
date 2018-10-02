db34f2b709e6239781981a6457b45d3cea30dcf1
William Antony <william@willsquad.com>
2018-10-01 21:44:28 -0500

    Added Form, CheckBox, Radio & Range to index.js

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,9 +1,19 @@
+import Form from './components/Form/Form';
+
 import Input from './components/Input/Input';
 import TextInput from './components/Input/components/TextInput/TextInput';
 import Select from './components/Input/components/Select/Select';
+import CheckBox from './components/Input/components/CheckBox/CheckBox';
+import Radio from './components/Input/components/Radio/Radio';
+import Range from './components/Input/components/Range/Range';
 
 export {
+  Form,
+
   Input,
   TextInput,
   Select,
+  CheckBox,
+  Radio,
+  Range,
 };
