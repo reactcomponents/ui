1ec5b35358413c70f8a333c156b469239285d329
William Antony <william@willsquad.com>
2018-10-16 13:32:39 -0500

    Refactored CurrentDateInfo Component; converted to stateless component

--- src/lib/components/Input/components/DateInput/components/DatePicker/components/CurrentDateInfo/CurrentDateInfo.js
+++ src/lib/components/Input/components/DateInput/components/DatePicker/components/CurrentDateInfo/CurrentDateInfo.js
@@ -1,54 +1,57 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import './CurrentDateInfo.css';
 
-class CurrentDateInfo extends Component {
-  constructor(props) {
-    super(props);
-    this.monthsList = [
-      'January',
-      'February',
-      'March',
-      'April',
-      'May',
-      'June',
-      'July',
-      'August',
-      'September',
-      'October',
-      'November',
-      'December',
-    ];
-    this.daysList = [
-      'Sunday',
-      'Monday',
-      'Tuesday',
-      'Wednesday',
-      'Thursday',
-      'Friday',
-      'Saturday',
-    ];
+const CurrentDateInfo = (props) => {
+  const {
+    date,
+    month,
+    year,
+    day,
+  } = props;
+
+  if ((date && month && year && day) === null) {
+    /**
+     * If any of the required prop is missing,
+     * then return null
+     */
+    return null;
   }
 
-  render() {
-    const {
-      date,
-      month,
-      year,
-      day,
-    } = this.props;
+  const daysOfWeek = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
 
-    return (
-      <div className="CurrentDateInfo">
-        <div className="CurrentDateInfo__date">
-          <div className="CurrentDateInfo__date__part">{ date }</div>/
-          <div className="CurrentDateInfo__date__part">{ this.monthsList[month] }</div>/
-          <div className="CurrentDateInfo__date__part">{ year }</div>
-        </div>
-        <div className="CurrentDateInfo__day">{ this.daysList[day] }</div>
+  return (
+    <div className="CurrentDateInfo">
+      <div className="CurrentDateInfo__date">
+        <div className="CurrentDateInfo__date__part">{ date }</div>/
+        <div className="CurrentDateInfo__date__part">{ month }</div>/
+        <div className="CurrentDateInfo__date__part">{ year }</div>
       </div>
-    );
-  }
+      <div className="CurrentDateInfo__day">{ daysOfWeek[day] }</div>
+    </div>
+  );
+};
 
-}
+CurrentDateInfo.propTypes = {
+  date: PropTypes.number,
+  month: PropTypes.string,
+  year: PropTypes.number,
+  day: PropTypes.number,
+};
+
+CurrentDateInfo.defaultProps = {
+  date: null,
+  month: null,
+  year: null,
+  day: null,
+};
 
 export default CurrentDateInfo;
